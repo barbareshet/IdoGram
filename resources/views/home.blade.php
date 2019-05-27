a4372492eab856426898241974b59e8e544f5e8a
ido barnea <barbareshet@gmail.com>
2019-05-27 11:06:40 +0300

    first commit
    files and folder structure

---
+++ resources/views/home.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 p-5">
+            <img src="https://instagram.fsdv2-1.fna.fbcdn.net/vp/9302bb0c6fc5046f1817f17e7073c27c/5D842C74/t51.2885-19/s150x150/31501736_418502788623078_7486417250090483712_n.jpg?_nc_ht=instagram.fsdv2-1.fna.fbcdn.net" alt="" class="rounded-circle">
+        </div>
+        <div class="col-md-9 p-5">
+            <div><h1>title</h1></div>
+            <div class="d-flex account-info">
+                <div class="pr-5"><strong>436</strong> posts</div>
+                <div class="pr-5"><strong>198</strong> followers</div>
+                <div class="pr-5"><strong>250</strong> following</div>
+            </div>
+            <div class="account-name pt-4"><strong>Ido Barnea</strong></div>
+            <div class="account-desc">
+                <p>
+                    לאהוב (את אשתי והילדים),
+                    לאכול (כמעט כל דבר)
+                    לצלם (כדי לתפוס את הרגעים האלה בחיים)
+                </p>
+            </div>
+            <div class="account-website pt-4">
+                <a href="#" rel="nofollow" target="_blank">
+                    www.barbareshet.co.il
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row p-4">
+        <div class="col-md-4">
+            <img src="https://instagram.fsdv2-1.fna.fbcdn.net/vp/dbd75b273c735b8f136ca9d18b65a8f0/5D838B7F/t51.2885-15/sh0.08/e35/s640x640/60021976_2791121660959302_2562435087434665850_n.jpg?_nc_ht=instagram.fsdv2-1.fna.fbcdn.net" alt="" class="w-100">
+        </div>
+        <div class="col-md-4">
+            <img src="https://instagram.fsdv2-1.fna.fbcdn.net/vp/dbd75b273c735b8f136ca9d18b65a8f0/5D838B7F/t51.2885-15/sh0.08/e35/s640x640/60021976_2791121660959302_2562435087434665850_n.jpg?_nc_ht=instagram.fsdv2-1.fna.fbcdn.net" alt="" class="w-100">
+        </div>
+        <div class="col-md-4">
+            <img src="https://instagram.fsdv2-1.fna.fbcdn.net/vp/dbd75b273c735b8f136ca9d18b65a8f0/5D838B7F/t51.2885-15/sh0.08/e35/s640x640/60021976_2791121660959302_2562435087434665850_n.jpg?_nc_ht=instagram.fsdv2-1.fna.fbcdn.net" alt="" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
